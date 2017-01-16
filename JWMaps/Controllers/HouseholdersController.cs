1f8a9e13062822f56c825063d2108b8527cb4626
gilliano <gilliano@DESKTOP-R0C0JHQ>
2017-01-16 20:57:31 -0300

    Create initial view to list householder's list

---
+++ JWMaps/Controllers/HouseholdersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JWMaps.Models;
+using JWMaps.ViewModel;
+
+namespace JWMaps.Controllers
+{
+    public class HouseholdersController : Controller
+    {
+        // GET: Householder
+        public ActionResult Index()
+        {
+            var householders = new List<Householder>
+            {
+                new Householder
+                {
+                    Name = "Juan gonzalez",
+                    Address = "Avenida Boa viagem, 455"
+                },
+
+                new Householder
+                {
+                    Name = "María Lopez",
+                    Address = "Avenida 17 de agosto, 1056"
+                }
+            };
+
+            var houseHolderViewModel = new HouseholderViewModel
+            {
+                Householders = householders
+            };
+
+            return View("ListHouseholders", houseHolderViewModel);
+        }
+    }
+}
---
+++ JWMaps/ViewModel/HouseholderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using JWMaps.Models;
+
+namespace JWMaps.ViewModel
+{
+    public class HouseholderViewModel
+    {
+        public IEnumerable<Householder> Householders { get; set; }        
+    }
+}
