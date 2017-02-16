a587c1991fbc26c68d80b5492dae0f5544dc25b4
gilliano <gilliano@DESKTOP-R0C0JHQ>
2017-02-15 21:43:08 -0300

    Initial version of dashboard

--- JWMaps/Controllers/HomeController.cs
+++ JWMaps/Controllers/HomeController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JWMaps.ViewModel;
+using JWMaps.Models;
 
 namespace JWMaps.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            DashboardViewModel dashboardViewModel = new DashboardViewModel
+            {
+                Householders = _context.Householders.ToList()
+            };
+
+            return View(dashboardViewModel);
         }
 
         public ActionResult About()
---
+++ JWMaps/ViewModel/DashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JWMaps.Models;
+
+namespace JWMaps.ViewModel
+{
+    public class DashboardViewModel
+    {
+        public List<Householder> Householders { get; set; }
+    }
+}
