87937bb9cac7098a4d3551b385100f3062551d21
Gilliano <Gilliano@LAPTOP-LS2EOVQ1>
2017-09-21 17:00:38 -0300

    Bug fix related to create new users

--- JWMaps/Controllers/AdminController.cs
+++ JWMaps/Controllers/AdminController.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.AspNet.Identity.EntityFramework;
 using JWMaps.Models;
 using PagedList;
-
 #endregion Includes
 
 namespace JWMaps.Controllers
@@ -104,16 +103,14 @@ namespace JWMaps.Controllers
         #region public ActionResult Create()
         public ActionResult Create()
         {
-            //ExpandedUserDTO objExpandedUserDTO = new ExpandedUserDTO();
+            ExpandedUserDTO objExpandedUserDTO = new ExpandedUserDTO();
 
-            //ViewBag.Roles = GetAllRolesAsSelectList();            
+            ViewBag.Roles = GetAllRolesAsSelectList();
 
-            //return View(objExpandedUserDTO);
-
-            return RedirectToAction("Register", "Account");
+            return View(objExpandedUserDTO);            
         }
         #endregion
-
+        
         [Authorize(Roles = RoleName.CanAdministrate)]
         public ActionResult Index()
         {
@@ -148,46 +145,39 @@ namespace JWMaps.Controllers
                 {
                     return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
                 }
-
                 var Email = paramExpandedUserDTO.Email.Trim();
-                var UserName = paramExpandedUserDTO.UserName.Trim();
+                var UserName = paramExpandedUserDTO.Email.Trim();
                 var Password = paramExpandedUserDTO.Password.Trim();
-
                 if (Email == "")
                 {
                     throw new Exception("No Email");
                 }
-
                 if (Password == "")
                 {
                     throw new Exception("No Password");
                 }
-                
+                // UserName is LowerCase of the Email
+                UserName = Email.ToLower();
                 // Create user
                 var objNewAdminUser = new ApplicationUser { UserName = UserName, Email = Email };
                 var AdminUserCreateResult = UserManager.Create(objNewAdminUser, Password);
-
-                return RedirectToAction("Index");
-
-                //if (AdminUserCreateResult.Succeeded == true)
-                //{
-                //    string strNewRole = Convert.ToString(Request.Form["Roles"]);
-
-                //    if (strNewRole != "0")
-                //    {
-                //        // Put user in role
-                //        UserManager.AddToRole(objNewAdminUser.Id, strNewRole);
-                //    }
-
-                //    return Redirect("~/Admin");
-                //}
-                //else
-                //{
-                //    ViewBag.Roles = GetAllRolesAsSelectList();
-                //    ModelState.AddModelError(string.Empty,
-                //        "Error: Failed to create the user. Check password requirements.");
-                //    return View(paramExpandedUserDTO);
-                //}
+                if (AdminUserCreateResult.Succeeded == true)
+                {
+                    string strNewRole = Convert.ToString(Request.Form["Roles"]);
+                    if (strNewRole != "0")
+                    {
+                        // Put user in role
+                        UserManager.AddToRole(objNewAdminUser.Id, strNewRole);
+                    }
+                    return Redirect("~/Admin");
+                }
+                else
+                {
+                    ViewBag.Roles = GetAllRolesAsSelectList();
+                    ModelState.AddModelError(string.Empty,
+                        "Error: Failed to create the user. Check password requirements.");
+                    return View(paramExpandedUserDTO);
+                }            
             }
             catch (Exception ex)
             {
